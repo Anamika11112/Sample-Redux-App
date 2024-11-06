245406bcc699ff142c2f2a203156524739159f1d
Anamika11112 <anamika@seemymachines.com>
2024-11-06 10:32:25 +0530

    Revert " basicRedux functionality added"

--- src/Redux/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import {countReducer,todoReducer} from './reducers'
-const rootReducer = combineReducers({
-    count: countReducer,
-    todos: todoReducer,
-  });
-  export default rootReducer;
