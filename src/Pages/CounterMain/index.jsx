977790b31ee98e65135ab18c2a90c9976ff6ab61
Anamika R S <anamika@seemymachines.com>
2024-11-06 09:56:09 +0530

    basicRedux functionality added

---
+++ src/Pages/CounterMain/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.css";
+import { useSelector } from "react-redux";
+function CounterMain() {
+  const count = useSelector((state) => state.count.count);
+  const todos = useSelector((state) => state.todos.todos);
+  return (
+    <div className="CounterMain_container">
+      <h1>{count}</h1>
+      <h4>Target Hours To Study</h4>
+      <h2>Subjects</h2>
+      <ul>
+        {todos.map((todos) => (
+          <li key={todos.id}>{todos.text.split(" ")[0]}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default CounterMain;
